7dc31f2620132d18796d8caab014acdf5e8e5a31
Szilard7593 <barthaszilard@icloud.com>
2026-01-02 18:16:04 +0200

    Bug done, enter is considered EOF

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 void print_clear(char *s,int len) {
     printf("|");
     for (int i = 0; i < len; i++) {
@@ -14,29 +15,40 @@ void print_clear(char *s,int len) {
     }
     printf("|");
 }
+
+//Probabil bug (While loop nu se termina niciodata deci nu putem afisa ultimi octeti
+//daca nu avem o linie de 2 octeti
+
 int main() {
     int character;
     int mem_index = 0;
     char *s = (char *)calloc(0x10, sizeof(char));
-    while ((character = getchar()) != EOF) {
-        s[mem_index % 0x10] = character;
+    while ((character = getchar()) != EOF && character != '\n') {
+
+        s[mem_index % 0x10] = (char)character;
         if (mem_index % 0x10 == 0x0) {
             printf("%08x ", mem_index);
         }
         printf("%02x ",character);
         mem_index++;
-        if (mem_index % 0x10 == 0x0) {
+        if ((mem_index % 0x10) == 0x0) {
 
             print_clear(s, 0x10);
             printf("\n");
             
         }
+
+
     }
+
     printf("%*s",3*(0x10 - (mem_index % 0x10)), " ");
     print_clear(s, (mem_index % 0x10));
-    printf("\n%08x",mem_index);
+    printf("\n%08x ",mem_index);
     printf("\n");
-    printf("Final memory dump:\n");
 
+    //printf("Finish\n");
 
+    free(s);
+    return 0;
 }
+
